16a8aefc86f4a46b6623494c849fe55cf8c0af15
Will Shields <43360999+wshields@users.noreply.github.com>
2019-11-05 16:36:56 -0500

    Added makeArray()

--- src/assignment/four/Main.java
+++ src/assignment/four/Main.java
@@ -1,8 +1,29 @@
 package assignment.four;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner key = new Scanner(System.in);
+        System.out.println("Enter an integer n: ");
+        int input = key.nextInt();
+        int[] arr = makeArray(input);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] makeArray(int x) {
+        int[] retArray = new int[x];
+        int num = 1;
+        for(int i = 0; i < retArray.length; i++) {
+            retArray[i] = num;
+            num ++;
+        }
+        return retArray;
+    }
+
+    public static void permute(int[] array) {
+        
     }
 }
